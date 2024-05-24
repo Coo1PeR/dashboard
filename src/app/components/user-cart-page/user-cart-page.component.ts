76b3f4d44d9f9633248f08f07c4914583c946018
Coo1PeR <coo2per@gmail.com>
2024-05-24 18:56:20 +0300

    feat: add button back

--- src/app/components/user-cart-page/user-cart-page.component.ts
+++ src/app/components/user-cart-page/user-cart-page.component.ts
@@ -1,12 +1,14 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {map, Observable} from "rxjs";
 import {UserFull} from "../../interfaces/interfaces";
-import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 import {Store} from "@ngxs/store";
 import {UsersState} from "../../store/users/users.state";
 import {MatProgressBar} from "@angular/material/progress-bar";
 import {AsyncPipe, NgIf} from "@angular/common";
 import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-user-cart-page',
@@ -16,7 +18,11 @@ import {MatCard, MatCardContent} from "@angular/material/card";
     AsyncPipe,
     NgIf,
     MatCard,
-    MatCardContent
+    MatCardContent,
+    MatGridList,
+    MatGridTile,
+    MatButton,
+    RouterLink
   ],
   templateUrl: './user-cart-page.component.html',
   styleUrl: './user-cart-page.component.scss'
@@ -24,6 +30,7 @@ import {MatCard, MatCardContent} from "@angular/material/card";
 export class UserCartPageComponent implements OnInit {
   private route = inject(ActivatedRoute)
   private store = inject(Store)
+  private router = inject(Router);
 
   user$!: Observable<UserFull | undefined>
 
@@ -32,5 +39,9 @@ export class UserCartPageComponent implements OnInit {
     this.user$ = this.store.select(UsersState.getUserFull).pipe(
       map(users => users.find(user => user.id === userId))
     );
+  }
+
+  goBack() {
+    this.router.navigate(['/main']);
   }
 }
