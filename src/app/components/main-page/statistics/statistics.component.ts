c3fa8c7c73fd9c0038aea22e2c76f350d39dde62
Coo1PeR <coo2per@gmail.com>
2024-05-29 11:13:27 +0300

    feat: add Apex charts

--- src/app/components/main-page/statistics/statistics.component.ts
+++ src/app/components/main-page/statistics/statistics.component.ts
@@ -1,8 +1,24 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
-import { StatisticsService } from '../../../services/statistics.service';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import {Component, OnInit, inject, ViewChild} from '@angular/core';
+import {CanvasJSAngularChartsModule} from '@canvasjs/angular-charts';
+import {StatisticsService} from '../../../services/statistics.service';
+import {RouterOutlet} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {
+  ChartComponent,
+  ApexOptions,
+  NgApexchartsModule,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
 
 @Component({
   selector: 'app-statistics',
@@ -10,15 +26,22 @@ import { CommonModule } from '@angular/common';
   imports: [
     CommonModule,
     RouterOutlet,
-    CanvasJSAngularChartsModule
+    CanvasJSAngularChartsModule,
+    NgApexchartsModule,
   ],
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss'],
 })
+
 export class StatisticsComponent implements OnInit {
   public productChartOptions: object | undefined;
   public userChartOptions: object | undefined;
   private statisticsService = inject(StatisticsService);
+
+  /////////////
+  @ViewChild("chart") chart: ChartComponent | undefined;
+  public productsChartOptionsApex: Partial<ChartOptions> | any
+  public usersChartOptionsApex: Partial<ChartOptions> | any
 
   productData: { productTitle: string, productTotalPurchase: number }[] = [];
   userData: { userFullName: string, userTotalPurchaseSum: number }[] = [];
@@ -26,12 +49,15 @@ export class StatisticsComponent implements OnInit {
   ngOnInit() {
     this.loadProductsData();
     this.loadUsersData();
+
+    ////////////////////////
   }
 
   private loadProductsData() {
     this.statisticsService.calculateProductRatio().subscribe(data => {
       this.productData = data;
       this.renderProductChart();
+      this.renderProductChartApex()
     });
   }
 
@@ -39,6 +65,8 @@ export class StatisticsComponent implements OnInit {
     this.statisticsService.calculateTotalPurchase().subscribe(data => {
       this.userData = data;
       this.renderUserChart();
+      this.renderUserChartApex();
+
     });
   }
 
@@ -49,7 +77,7 @@ export class StatisticsComponent implements OnInit {
       },
       data: [{
         type: 'column',
-        dataPoints: this.productData.map(item => ({ label: item.productTitle, y: item.productTotalPurchase }))
+        dataPoints: this.productData.map(item => ({label: item.productTitle, y: item.productTotalPurchase}))
       }]
     };
   }
@@ -61,8 +89,50 @@ export class StatisticsComponent implements OnInit {
       },
       data: [{
         type: 'column',
-        dataPoints: this.userData.map(item => ({ label: item.userFullName, y: item.userTotalPurchaseSum }))
+        dataPoints: this.userData.map(item => ({label: item.userFullName, y: item.userTotalPurchaseSum}))
       }]
     };
+  }
+
+  /////////////////
+  public renderProductChartApex() {
+    this.statisticsService.calculateProductRatio().subscribe(data => {
+      this.productsChartOptionsApex = {
+        series: [{
+          data: data.map(item => item.productTotalPurchase)
+        }],
+        chart: {
+          height: 350,
+          type: 'bar'
+        },
+        title: {
+          text: 'Соотношение купленных всех видов товаров и их количества'
+        },
+        xaxis: {
+          categories: data.map(item => item.productTitle)
+        }
+      };
+    });
+  }
+
+  public renderUserChartApex() {
+    this.statisticsService.calculateTotalPurchase().subscribe(data => {
+      this.usersChartOptionsApex = {
+        series: [{
+          title: 'Сумма покупок',
+          data: data.map(item => item.userTotalPurchaseSum)
+        }],
+        chart: {
+          height: 350,
+          type: 'bar'
+        },
+        title: {
+          text: 'Пользователи и их общая сумма покупок'
+        },
+        xaxis: {
+          categories: data.map(item => item.userFullName)
+        }
+      };
+    });
   }
 }
