1162b7dbebd07a843d2dfd1f6590bc0df84cff11
Coo1PeR <coo2per@gmail.com>
2024-05-30 00:20:35 +0300

    fix: refactor statistics component and remove unused statistics service

--- src/app/components/main-page/statistics/statistics.component.ts
+++ src/app/components/main-page/statistics/statistics.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit, inject, ViewChild} from '@angular/core';
-import {StatisticsService} from '../../../services/statistics.service';
 import {RouterOutlet} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {
@@ -10,6 +9,13 @@ import {
   ApexXAxis,
   ApexTitleSubtitle
 } from "ng-apexcharts";
+import {combineLatest, Observable} from "rxjs";
+import {map} from "rxjs/operators";
+import {Select, Store} from "@ngxs/store";
+import {CartsState} from "../../../core/stores/carts/carts.state";
+import {Cart, Product, UserFull} from "../../../interfaces/interfaces";
+import {ProductsState} from "../../../core/stores/products/products.state";
+import {UsersState} from "../../../core/stores/users/users.state";
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
@@ -29,78 +35,108 @@ export type ChartOptions = {
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss'],
 })
-
 export class StatisticsComponent implements OnInit {
-  private statisticsService = inject(StatisticsService);
+  @Select(CartsState.Carts) carts$!: Observable<Cart[]>;
+  @Select(ProductsState.Products) products$!: Observable<Product[]>;
+  private store = inject(Store);
 
-  /////////////
   @ViewChild("chart") chart: ChartComponent | undefined;
-  public productsChartOptionsApex: Partial<ChartOptions> | any
-  public usersChartOptionsApex: Partial<ChartOptions> | any
+  public productsChartOptionsApex: Partial<ChartOptions> | any;
+  public usersChartOptionsApex: Partial<ChartOptions> | any;
 
   productData: { productTitle: string, productTotalPurchase: number }[] = [];
   userData: { userFullName: string, userTotalPurchaseSum: number }[] = [];
 
   ngOnInit() {
-    this.loadProductsData();
-    this.loadUsersData();
-
-    ////////////////////////
+    this.loadProductData();
+    this.loadUserData();
   }
 
-  private loadProductsData() {
-    this.statisticsService.calculateProductRatio().subscribe(data => {
+  private loadProductData() {
+    this.calculateProductRatio().subscribe(data => {
       this.productData = data;
-      this.renderProductChartApex()
+      this.renderProductChartApex(data);
     });
   }
 
-  private loadUsersData() {
-    this.statisticsService.calculateTotalPurchase().subscribe(data => {
+  private loadUserData() {
+    this.calculateTotalPurchase().subscribe(data => {
       this.userData = data;
-      this.renderUserChartApex();
+      this.renderUserChartApex(data);
     });
   }
 
-  /////////////////
-  public renderProductChartApex() {
-    this.statisticsService.calculateProductRatio().subscribe(data => {
-      this.productsChartOptionsApex = {
-        series: [{
-          data: data.map(item => item.productTotalPurchase)
-        }],
-        chart: {
-          height: 350,
-          type: 'bar'
-        },
-        title: {
-          text: 'Соотношение купленных всех видов товаров и их количества'
-        },
-        xaxis: {
-          categories: data.map(item => item.productTitle)
-        }
+  public renderProductChartApex(data: { productTitle: string, productTotalPurchase: number }[]) {
+    this.productsChartOptionsApex = {
+      series: [{
+        data: data.map(item => item.productTotalPurchase)
+      }],
+      chart: {
+        height: 350,
+        type: 'bar'
+      },
+      title: {
+        text: 'Соотношение купленных всех видов товаров и их количества'
+      },
+      xaxis: {
+        categories: data.map(item => item.productTitle)
+      }
+    };
+  }
+
+  public renderUserChartApex(data: { userFullName: string, userTotalPurchaseSum: number }[]) {
+    this.usersChartOptionsApex = {
+      series: [{
+        title: 'Сумма покупок',
+        data: data.map(item => item.userTotalPurchaseSum)
+      }],
+      chart: {
+        height: 350,
+        type: 'bar'
+      },
+      title: {
+        text: 'Пользователи и их общая сумма покупок'
+      },
+      xaxis: {
+        categories: data.map(item => item.userFullName)
+      }
+    };
+  }
+
+  calculateProductRatio(): Observable<{ productTitle: string, productTotalPurchase: number }[]> {
+    return combineLatest([this.carts$, this.products$]).pipe(
+      map(([carts, products]) => this.calculateProductTotals(carts, products))
+    );
+  }
+
+  private calculateProductTotals(carts: Cart[], products: Product[]): { productTitle: string, productTotalPurchase: number }[] {
+    const productMap = carts.reduce((acc, cart) => {
+      cart.products.forEach(product => {
+        acc.set(product.productId, (acc.get(product.productId) || 0) + product.quantity);
+      });
+      return acc;
+    }, new Map<number, number>());
+
+    return Array.from(productMap.entries()).map(([productId, productTotalPurchase]) => {
+      const product = products.find(p => p.id === productId);
+      return {
+        productTitle: product ? product.title : 'Unknown Product',
+        productTotalPurchase: productTotalPurchase
       };
     });
   }
 
-  public renderUserChartApex() {
-    this.statisticsService.calculateTotalPurchase().subscribe(data => {
-      this.usersChartOptionsApex = {
-        series: [{
-          title: 'Сумма покупок',
-          data: data.map(item => item.userTotalPurchaseSum)
-        }],
-        chart: {
-          height: 350,
-          type: 'bar'
-        },
-        title: {
-          text: 'Пользователи и их общая сумма покупок'
-        },
-        xaxis: {
-          categories: data.map(item => item.userFullName)
-        }
-      };
-    });
+  // Метод для подсчета общей суммы покупок пользователей
+  calculateTotalPurchase(): Observable<{ userFullName: string, userTotalPurchaseSum: number }[]> {
+    // TODO check takeUntilDestroyed
+    // TODO check stores.selectSnapshot
+    return this.store.select(UsersState.Users).pipe(
+      map((users: UserFull[]) => {
+        return users.map(user => ({
+          userFullName: user.userFullName,
+          userTotalPurchaseSum: user.totalPurchase
+        }));
+      })
+    );
   }
 }
