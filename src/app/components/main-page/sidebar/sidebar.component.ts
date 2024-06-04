d275446725321e342688f7e6d9584dff4f1110fb
Coo1PeR <coo2per@gmail.com>
2024-06-04 17:45:55 +0300

    feat: create custom theme; not implemented yet

--- src/app/components/main-page/sidebar/sidebar.component.ts
+++ src/app/components/main-page/sidebar/sidebar.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, HostListener, inject, OnInit} from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 import {MatListItem, MatNavList} from "@angular/material/list";
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {MatDivider} from "@angular/material/divider";
 import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {OverlayContainer} from "@angular/cdk/overlay";
 
 @Component({
   selector: 'app-sidebar',
@@ -15,11 +17,31 @@ import {MatSlideToggle} from "@angular/material/slide-toggle";
     RouterLink,
     RouterLinkActive,
     MatDivider,
-    MatSlideToggle
+    MatSlideToggle,
+    ReactiveFormsModule
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
+  private overlay = inject(OverlayContainer);
 
+  switchTheme = new FormControl(false)
+  @HostBinding('class') className = ''
+  darkClass = 'theme-dark'
+  lightClass = 'theme-light'
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.switchTheme.valueChanges.subscribe(currentMode => {
+      this.className = currentMode? this.darkClass : this.lightClass;
+      if(currentMode) {
+        this.overlay.getContainerElement().classList.add(this.darkClass);
+      } else {
+        this.overlay.getContainerElement().classList.remove(this.darkClass);
+      }
+    })
+  }
 }
