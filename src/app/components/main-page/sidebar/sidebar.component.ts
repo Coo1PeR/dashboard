739b4b423445afa1b1810917aa992a12c8e32ecc
Coo1PeR <coo2per@gmail.com>
2024-06-05 11:32:44 +0300

    feat: implement custom theme

--- src/app/components/main-page/sidebar/sidebar.component.ts
+++ src/app/components/main-page/sidebar/sidebar.component.ts
@@ -1,47 +1,56 @@
-import {Component, HostBinding, inject, OnInit} from '@angular/core';
-import {MatIcon} from "@angular/material/icon";
-import {MatListItem, MatNavList} from "@angular/material/list";
-import {RouterLink, RouterLinkActive} from "@angular/router";
-import {MatDivider} from "@angular/material/divider";
-import {MatSlideToggle} from "@angular/material/slide-toggle";
-import {FormControl, ReactiveFormsModule} from "@angular/forms";
-import {OverlayContainer} from "@angular/cdk/overlay";
+import { Component, HostBinding, inject, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
-    MatIcon,
-    MatListItem,
-    MatNavList,
+    MatIconModule,
+    MatListModule,
     RouterLink,
     RouterLinkActive,
-    MatDivider,
-    MatSlideToggle,
+    MatDividerModule,
+    MatSlideToggleModule,
     ReactiveFormsModule
   ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit{
+export class SidebarComponent implements OnInit {
   private overlay = inject(OverlayContainer);
 
-  switchTheme = new FormControl(false)
-  @HostBinding('class') className = ''
-  darkClass = 'theme-dark'
-  lightClass = 'theme-light'
+  switchTheme = new FormControl(false);
+  @HostBinding('class') className = '';
+  darkClass = 'dark-theme';
+  lightClass = 'light-theme';
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit() {
     this.switchTheme.valueChanges.subscribe(currentMode => {
-      this.className = currentMode? this.darkClass : this.lightClass;
-      if(currentMode) {
-        this.overlay.getContainerElement().classList.add(this.darkClass);
+      this.className = currentMode ? this.darkClass : this.lightClass;
+      const overlayContainerClasses = this.overlay.getContainerElement().classList;
+      if (currentMode) {
+        overlayContainerClasses.add(this.darkClass);
+        overlayContainerClasses.remove(this.lightClass);
       } else {
-        this.overlay.getContainerElement().classList.remove(this.darkClass);
+        overlayContainerClasses.add(this.lightClass);
+        overlayContainerClasses.remove(this.darkClass);
       }
-    })
+    });
+    // Set initial theme
+    this.className = this.switchTheme.value ? this.darkClass : this.lightClass;
+    const overlayContainerClasses = this.overlay.getContainerElement().classList;
+    if (this.switchTheme.value) {
+      overlayContainerClasses.add(this.darkClass);
+    } else {
+      overlayContainerClasses.add(this.lightClass);
+    }
   }
 }
