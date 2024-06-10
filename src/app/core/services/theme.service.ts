8af54e129b5e323a4ae909bd0b801af811ae219a
Coo1PeR <coo2per@gmail.com>
2024-06-10 12:44:47 +0300

    feat: add type of theme to localStorage

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,16 +1,21 @@
 import {Injectable, signal} from '@angular/core';
 
+let themeStorage = localStorage.getItem('theme') === 'light' ? 'light' : 'dark';
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class ThemeService {
-  themeSignal = signal<string>('dark')
+
+  themeSignal = signal<string>(themeStorage)
 
   setTheme(theme: string) {
     this.themeSignal.set(theme)
   }
 
   updateTheme() {
-    this.themeSignal.update((value) => (value === 'dark' ? 'light' : 'dark'))
+    this.themeSignal.update((value) => (value === 'light' ? 'dark' : 'light'))
+    localStorage.setItem('theme', this.themeSignal())
   }
 }
